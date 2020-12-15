19374e8e3d123f253150af0a402ea3779b63896c
sina <sinabahramloo@gmail.com>
2020-12-15 12:28:30 +0330

    overload printArray method for subscription

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,16 @@ public class Main {
             System.out.println(i.toString());
         }
     }
+    public static <T> void printArray(T[]array,int lowSubscript,int highSubscript){
+        if(lowSubscript >= 0  && lowSubscript<=highSubscript && highSubscript<= array.length){
+        for (int i=lowSubscript;i<highSubscript;i++) {
+            System.out.println(array[i].toString());
+        }
+        }
+        else {
+            throw new IllegalArgumentException("lowSubscript and highSubscript must be in array rnge and lowSubscript must be lower thanhighSubscript");
+        }
+    }
     public static void main(String[] args) {
         Integer [] integerArray={1,2,3};
         Double [] doubleArray={1.1,2.1,3.1};
